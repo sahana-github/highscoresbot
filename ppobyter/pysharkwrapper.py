143bfec4d994978f1a706de9fa0b03af96c58b98
graatje <42679597+graatje@users.noreply.github.com>
2022-02-09 15:49:29 +0100

    new way to get packets, will get tested on live with backup on main available

--- ppobyter/pysharkwrapper.py
+++ ppobyter/pysharkwrapper.py
@@ -5,8 +5,8 @@ class PysharkWrapper:
     def __init__(self):
         self.__cap = pyshark.LiveCapture(interface="Ethernet 2", include_raw=True, use_json=True, display_filter="ip.src == 167.114.159.20")
 
-        #self.__cap = pyshark.LiveCapture(interface=r"\Device\NPF_{65E2C297-AC80-4851-95C2-795C9783D00F}", include_raw=True,
-         #                                use_json=True, display_filter="ip.src == 167.114.159.20")
+       # self.__cap = pyshark.LiveCapture(interface=r"\Device\NPF_{65E2C297-AC80-4851-95C2-795C9783D00F}", include_raw=True,
+        #                                 use_json=True, display_filter="ip.src == 167.114.159.20")
     @staticmethod
     def decodehex(hexa: str) -> str:
         """
@@ -24,14 +24,17 @@ class PysharkWrapper:
         return result
 
     def cap(self):
+        currentmsg = ""
         for packet in self.__cap.sniff_continuously():
             try:
                 hexa = packet.tcp.payload.replace(":", "")
                 r = PysharkWrapper.decodehex(hexa)
-                for msg in r.split("\x00"):
-                    if msg == "":
-                        continue
-                    yield msg
+                for character in r:
+                    if character == "\x00":
+                        yield currentmsg
+                        currentmsg = ""
+                    else:
+                        currentmsg += character
 
             except AttributeError:
                 continue
