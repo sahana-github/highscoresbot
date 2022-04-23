5effee61bbed8d04189ac0f54adc4f7b38d41433
graatje <42679597+graatje@users.noreply.github.com>
2022-04-23 12:12:18 +0200

    a few highscores commands take sendables now.

--- commands/actual_commands/highscores.py
+++ commands/actual_commands/highscores.py
@@ -1,3 +1,6 @@
+import sqlite3
+from typing import Union
+
 from discord import app_commands, Interaction, InteractionResponse
 from discord.ext import commands
 
@@ -8,6 +11,7 @@ from commands.sendable import Sendable
 class Highscores(commands.Cog):
     def __init__(self, client):
         self.client: commands.bot.Bot = client
+        self.databasepath = "highscores.db"
 
     highscoresgroup = app_commands.Group(name="highscores",
                                          description="all commands that have to do directly with all the highscores of pokemon planet.")
@@ -23,6 +27,42 @@ class Highscores(commands.Cog):
         #await interaction.response.pong()
         #await interaction.response.defer(thinking=False)
         await highscores.getclan(Sendable(interaction), clanname)
+
+    @highscoresgroup.command(name="top")
+    async def top(self, interaction: Interaction, clanname: str=None):
+        sendable = Sendable(interaction)
+        if clanname is None:
+            clanname = await self.getdefaultclanname(sendable, comment=False)
+        await highscores.top(sendable, clanname)
+
+    @highscoresgroup.command(name="highscore")
+    async def highscore(self, interaction: Interaction, clanname: str=None):
+        sendable = Sendable(interaction)
+        await highscores.highscore(sendable, clanname)
+
+    @highscoresgroup.command(name="mapcontrol")
+    async def mapcontrol(self, interaction: Interaction, clanname: str=None):
+        sendable = Sendable(interaction)
+        if clanname is None and ((clanname := await self.getdefaultclanname(sendable)) is None):
+            clanname = ""
+        await highscores.mapcontrol(sendable, clanname)
+
+    async def getdefaultclanname(self, sendable: Sendable, comment=True) -> Union[str, None]:
+        if sendable.guild is None:
+            return
+        conn = sqlite3.connect(self.databasepath)
+        cur = conn.cursor()
+        cur.execute("SELECT name FROM clannames WHERE id=?", (sendable.guild.id,))
+        try:
+            clanname = cur.fetchall()[0][0]
+        except IndexError:
+            clanname = None
+        if clanname is None and comment:
+            await sendable.send("Please register a default clanname or provide a clan in the command.")
+        elif clanname is not None:
+            clanname = clanname.lower()
+        return clanname
+
 
 async def setup(client: commands.Bot):
     await client.add_cog(Highscores(client))
--- commands/command_functionality/highscores.py
+++ commands/command_functionality/highscores.py
@@ -1,6 +1,11 @@
 import sqlite3
 
+from discord import Interaction
+
+from commands.interractions.highscore_command import HighscoreCommand
 from commands.interractions.resultmessageshower import ResultmessageShower
+from commands.interractions.selectsview import SelectsView
+from commands.interractions.top_command import TopCommand
 from commands.sendable import Sendable
 from commands.utils.utils import joinmessages, tablify
 from highscores import allhighscores, AncMapcontrol, BzMapcontrol, BestClans, RichestClans, SafariMapcontrol
@@ -56,3 +61,57 @@ async def getclan(sendable: Sendable, clanname: str):
 
     view = ResultmessageShower(allmessages, sendable)
     await sendable.send(allmessages[0], view=view)
+
+
+async def top(sendable: Sendable, clanname: str=None):
+    """
+    shows top 9 + the provided clan if available.
+    :param ctx: discord context
+    :param clanname: the clanname, default none, clannamehandler gets clan from db if none.
+    """
+    highscoresdict = {}
+    for highscore in allhighscores:
+        highscore = highscore()
+        highscoresdict[highscore.NAME] = highscore
+
+    def highscoreselectionmaker(highscores):
+        return TopCommand(sendable, highscores, clanname)
+
+    view = SelectsView(sendable, highscoresdict, highscoreselectionmaker)
+    await sendable.send(content=f"page {view.currentpage} of {view.maxpage}", view=view)
+
+
+async def highscore(sendable: Sendable, clanname: str=None):
+    initializedhighscores = {}
+    for highscore in allhighscores:
+        highscore = highscore()
+        initializedhighscores[highscore.NAME] = highscore
+
+    def highscoreselectionmaker(highscores):
+        return HighscoreCommand(sendable, highscores, clanname=clanname)
+
+    view = SelectsView(sendable, initializedhighscores.keys(), highscoreselectionmaker)
+    await sendable.send(content=f"page {view.currentpage} of {view.maxpage}", view=view)
+
+
+async def mapcontrol(sendable: Sendable, clanname: str=None):
+    """
+    shows the standings of all mapcontrol areas.
+    :param ctx: discord context
+    :param clanname: the name of the clan, optional.
+    """
+    mapcontrolhighscores = [(AncMapcontrol, "Ancient cave"), (BzMapcontrol, "Battle zone"),
+                            (SafariMapcontrol, "Safari zone")]
+    if clanname is None:
+        clanname = ""
+    messages = []
+    for highscore, area in mapcontrolhighscores:
+        highscore = highscore()
+        values = tablify(highscore.LAYOUT, highscore.getDbValues(
+            query=f"SELECT * FROM {highscore.NAME} WHERE rank < 10 or clan=?",
+            clan=clanname.lower()))
+        messages.append(area)
+        messages += values
+    messages = joinmessages(messages)
+    view = ResultmessageShower(messages, sendable)
+    await sendable.send(messages[0], view=view)
