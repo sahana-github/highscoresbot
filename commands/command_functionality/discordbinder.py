9c4f3129ad0d0a7fd26b201ba2f0294aff0cdc46
graatje <42679597+graatje@users.noreply.github.com>
2022-04-24 23:22:19 +0200

    some references to commands

--- commands/command_functionality/discordbinder.py
+++ commands/command_functionality/discordbinder.py
@@ -18,11 +18,12 @@ async def bind(sendable: Sendable, accountname: str, userid: int):
                           "would be sent to your discord account.", inline=False)
     embed.add_field(name="this wasn't me",
                     value="you could deny this specific ppo user from binding with your discord account,"
-                          " and he won't be able to send future requests to bind to your user account (you can undo this with a command on discord).",
+                          " and he won't be able to send future requests to bind to your user account (you can undo this with "
+                          "`/discord_binding_group unblock <ppousername>",
                     inline=False)
     embed.add_field(name="I don't want any message about this in the future",
                     value="you can press `prevent all accountbinding in the future` if you don't want any messages"
-                          " like this one at all anymore. This can be undone with `{future_command}` in discord",
+                          " like this one at all anymore. This can be undone with `/discord_binding_group unblockall` in discord",
                     inline=False)
     view = DiscordBinder(accountname, userid)
     await sendable.send(embed=embed, view=view)
