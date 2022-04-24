ae9a96c31840eeb38b985676d81f9aa4bfb86a64
graatje <42679597+graatje@users.noreply.github.com>
2022-04-24 18:12:52 +0200

    functionality for discordbindings.

--- commands/command_functionality/discordbinder.py
+++ commands/command_functionality/discordbinder.py
@@ -1,8 +1,13 @@
+import sqlite3
+
 import discord
 from discord import User
 
 from commands.sendable import Sendable
 from commands.interractions.discord_binder import DiscordBinder
+
+
+database = "eventconfigurations.db"
 
 
 async def bind(sendable: Sendable, accountname: str, userid: int):
@@ -21,3 +26,45 @@ async def bind(sendable: Sendable, accountname: str, userid: int):
                     inline=False)
     view = DiscordBinder(accountname, userid)
     await sendable.send(embed=embed, view=view)
+
+
+async def showbindings(sendable: Sendable, userid: int):
+    with sqlite3.connect(database) as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT pponame FROM discord_bindings WHERE discordid=?", (userid,))
+        pponames = [row[0] for row in cur.fetchall()]
+    await sendable.send("Your discord account has been bound to the following ppo players: \n`" +
+                        "\n".join(pponames) + "`")
+
+
+async def removebinding(sendable: Sendable, ppousername: str, userid: int):
+    with sqlite3.connect(database) as conn:
+        cur = conn.cursor()
+        result = cur.execute("DELETE FROM discord_bindings WHERE userid=? AND pponame=?", (userid, ppousername))
+        conn.commit()
+    if result.rowcount:
+        await sendable.send(f"Binding with player {ppousername} removed!")
+    else:
+        await sendable.send(f"Unable to unbind with {ppousername}, you might not have a binding with that user.")
+
+
+async def unblock(sendable: Sendable, ppousername: str, userid: int):
+    with sqlite3.connect(database) as conn:
+        cur = conn.cursor()
+        result = cur.execute("DELETE FROM discord_blocked WHERE pponame=? AND discordid=?", (ppousername, userid))
+        conn.commit()
+    if result.rowcount:
+        await sendable.send(f"Unblocked {ppousername} from binding!")
+    else:
+        await sendable.send(f"unable to unblock {ppousername}. The player might not have been blocked at all.")
+
+
+async def unblockall(sendable: Sendable, userid: int):
+    with sqlite3.connect(database) as conn:
+        cur = conn.cursor()
+        result = cur.execute("DELETE FROM everything_discord_blocked WHERE discordid=?", (userid,))
+        conn.commit()
+    if result.rowcount:
+        await sendable.send("you undid blocking everyone from binding with your account.")
+    else:
+        await sendable.send("You didn't block everyone from binding with your account.")
